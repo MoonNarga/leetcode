8b735e65963dca934dcc3185dadd6da15fd4ba5d
MoonNarga <MoonNarga@outlook.com>
2021-11-20 12:28:14 +0800

    594 done

---
+++ 594.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+func findLHS(nums []int) int {
+	numSet := make(map[int]int)
+	length := 0
+	for _, v := range nums {
+		if _, ok := numSet[v]; ok {
+			numSet[v] += 1
+		} else {
+			numSet[v] = 1
+		}
+	}
+	for i, v := range numSet {
+		if val, ok := numSet[i-1]; ok {
+			if val+v > length {
+				length = val+v
+			}
+		}
+	}
+	return length
+}
+
+func main() {
+	n := []int{1, 2, 3, 4}
+	fmt.Println(findLHS(n))
+}
