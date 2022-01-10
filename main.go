d7943813a80f4222ccfed8c6666a13d5c26cebbd
MoonNarga <MoonNarga@outlook.com>
2022-01-11 00:01:03 +0800

    update

--- main.go
+++ main.go
@@ -2,10 +2,20 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func isAdditiveNumber(num string) bool {
-	
+	numArray := []byte(num)
+	lmax := len(num) / 3
+	var tempNum1, tempNum2, tempNum3, tempsum int
+	for l1 := 1; l1 <= lmax; l1++ {
+		tempNum1, _ = strconv.Atoi(string(numArray[0:l1]))
+		for l2 := 1; l2 <= lmax; l2++ {
+			tempNum2, _ = strconv.Atoi(string(numArray[l1 : l1+l1]))
+			tempsum = tempNum1 + tempNum2
+		}
+	}
 	return false
 }
 
