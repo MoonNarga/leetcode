2a8eddfd9c65c2a6df90ba9726370ec25f0607fc
MoonNarga <MoonNarga@outlook.com>
2022-01-10 13:03:40 +0800

    update

--- main.go
+++ main.go
@@ -4,8 +4,12 @@ import (
 	"fmt"
 )
 
+func isAdditiveNumber(num string) bool {
+	
+	return false
+}
+
 func main() {
-	keysPressed := "spuda"
-	releaseTimes := []int{12,23,36,46,62}
-	fmt.Printf("%c\n", slowestKey(releaseTimes, keysPressed))
+	num := "112358"
+	fmt.Println(isAdditiveNumber(num))
 }
