4fc4cdd57a10ab308115986ad5627dbef7960604
MoonNarga <MoonNarga@outlook.com>
2022-07-31 17:17:24 +0800

    1161 done

---
+++ solution/1161.go
@@ -0,0 +1,47 @@
+package solution
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func maxLevelSum(root *TreeNode) int {
+	if root == nil {
+		return 1
+	}
+	nodeNumOfLevel := 1
+	tempNodeNum := 0
+	maxSum := root.Val
+	maxLevel := 1
+	currentLevel := 1
+	tempSum := 0
+	levelList := make([]*TreeNode, 0)
+	levelList = append(levelList, root)
+	for ; nodeNumOfLevel != 0; nodeNumOfLevel-- {
+		tempSum += levelList[0].Val
+		if levelList[0].Left != nil {
+			levelList = append(levelList, levelList[0].Left)
+			tempNodeNum++
+		}
+		if levelList[0].Right != nil {
+			levelList = append(levelList, levelList[0].Right)
+			tempNodeNum++
+		}
+		levelList = levelList[1:]
+		if nodeNumOfLevel == 1 {
+			if maxSum < tempSum {
+				maxSum = tempSum
+				maxLevel = currentLevel
+			}
+			if tempNodeNum != 0 {
+				nodeNumOfLevel, tempNodeNum = tempNodeNum+1, 0
+				tempSum = 0
+				currentLevel++
+			}
+		}
+	}
+	return maxLevel
+}
