3bb93dcbf715697dc01de789f37f6cccfabedd7d
MoonNarga <MoonNarga@outlook.com>
2022-08-11 12:18:34 +0800

    rewrite 1417

--- solution/1417.go
+++ solution/1417.go
@@ -3,9 +3,7 @@ package solution
 func reformat(s string) string {
 	str := []byte(s)
 	length := len(str)
-	res := make([]byte, length)
 	coNum, coLe := 0, 0
-	num, letter := 0, 0
 	for _, v := range str {
 		if v > '9' {
 			coLe++
@@ -16,19 +14,28 @@ func reformat(s string) string {
 	if coLe-coNum > 1 || coLe-coNum < -1 {
 		return ""
 	}
-	if coNum >= coLe {
-		num, letter = 0, 1
+	if coLe >= coNum {
+		for l, r := 0, 1; r < length; l, r = l+2, r+2 {
+			for ; r < length && str[r] <= '9'; r += 2 {
+			}
+			if r >= length {
+				break
+			}
+			for ; l < length && str[l] > '9'; l += 2 {
+			}
+			str[l], str[r] = str[r], str[l]
+		}
 	} else {
-		num, letter = 1, 0
-	}
-	for _, v := range str {
-		if v > '9' {
-			res[letter] = v
-			letter += 2
-		} else {
-			res[num] = v
-			num += 2
+		for l, r := 1, 0; r < length; l, r = l+2, r+2 {
+			for ; l < length && str[l] > '9'; l += 2 {
+			}
+			if l >= length {
+				break
+			}
+			for ; r < length && str[r] <= '9'; r += 2 {
+			}
+			str[l], str[r] = str[r], str[l]
 		}
 	}
-	return string(res)
+	return string(str)
 }
